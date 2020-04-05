9c1651dea32580511d87ba6be642f8df6cf9880e
Daniel-Osei <demajesty.6@gmail.com>
2020-04-05 09:03:26 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Web Dev tutorials for Jonas
---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function($) {
+    var typed  = new Typed('span.txt-rotate', {
+        strings: ["I'm a Web Developer", "I'm a Photographer", "I'm an Artist"],
+        typeSpeed: 50,
+        backSpeed: 100,
+        fadeOut: false,
+        smartBackspace: true,
+        loop: true,
+    })
+})(jQuery);
